653ffb97d77c398a52f19f8f401cfdbaea086a53
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-15 23:46:28 +0400

    Commit 150324

--- task5.py
+++ task5.py
@@ -1,1 +1,19 @@
-print('hello')
+class Nikola:
+    __slots__ = ('name', 'age')
+
+    def __init__(self, name, age):
+        if name != 'Николай':
+            self.name = f'Я не {name}, а Николай'
+        else:
+            self.name = 'Николай'
+        self.age = age
+
+# Тесты
+person1 = Nikola('Иван', 31)
+person2 = Nikola('Николай', 14)
+
+print(person1.name)  # Выведет: Я не Иван, а Николай
+print(person2.name)  # Выведет: Николай
+
+# Попытка добавить новый атрибут 'surname' к объекту person2 приведет к ошибке
+"""person2.surname = 'Петров' """
