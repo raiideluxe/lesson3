0c1b30f2a80c40e04f5bf7cff6a35554f1183d05
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-15 23:32:30 +0400

    Commit 150324

--- task2.py
+++ task2.py
@@ -1,1 +1,25 @@
-print('hello')
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} сел(а).")
+
+    def roll_over(self):
+        print(f"{self.name} перекатывается.")
+
+
+my_dog = Dog('Willie', 6)
+print(f"Имя собаки: {my_dog.name}")
+print(f"Возраст собаки: {my_dog.age}")
+
+my_dog.sit()
+my_dog.roll_over()
+
+second_dog = Dog('Lucy', 3)
+print(f"\nИмя второй собаки: {second_dog.name}")
+print(f"Возраст второй собаки: {second_dog.age}")
+
+second_dog.sit()
+second_dog.roll_over()
